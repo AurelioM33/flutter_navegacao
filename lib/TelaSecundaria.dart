8047b4b3378af78ac9819ea2e0b6d6212f9ab37f
AurelioM33 <m.aurelio887@gmail.com>
2022-04-26 20:46:53 -0300

    primeiro commit

---
+++ lib/TelaSecundaria.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class TelaSecundaria extends StatefulWidget {
+  String valor;
+
+  TelaSecundaria(this.valor);
+
+  @override
+  State<TelaSecundaria> createState() => _TelaSecundariaState();
+}
+
+class _TelaSecundariaState extends State<TelaSecundaria> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tela Secundaria"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Text("Segunda tela! valor passado: ${widget.valor}")
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter/material.dart';
+import 'package:navegacao/TelaSecundaria.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: TelaPrincipal(),
+  ));
+}
+
+class TelaPrincipal extends StatefulWidget {
+  const TelaPrincipal({Key key}) : super(key: key);
+
+  @override
+  State<TelaPrincipal> createState() => _TelaPrincipalState();
+}
+
+class _TelaPrincipalState extends State<TelaPrincipal> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tela Principal"),
+        backgroundColor: Colors.orange,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(32),
+        child: Column(
+          children: <Widget>[
+            RaisedButton(
+              child: Text("Ir para a segunda tela"),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => TelaSecundaria("Marco Aurélio")));
+              },
+              padding: EdgeInsets.all(15),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
